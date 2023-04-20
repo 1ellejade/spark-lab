f5ebedaf1ecf41d9e78384410e84cf31040651cb
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-04-20 12:30:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-python3 /spark-examples/setup.py
+python3 /spark-lab/setup.py
 cat workers | while read line
 do
     if [ "$line" = "-" ]; then
         echo "Skip $line"
     else
-        ssh root@$line -n "rm -rf /spark-examples/ && mkdir /spark-examples/"
+        ssh root@$line -n "rm -rf /spark-lab/ && mkdir /spark-lab/"
         echo "Copy data to $line"
-        scp /spark-examples/hadoop-3.3.1.tar.gz root@$line:/spark-examples/ &&  scp /spark-examples/spark-3.2.1-bin-hadoop3.2.tgz root@$line:/spark-examples/ && scp /spark-examples/setup.py root@$line:/spark-examples/ && scp /spark-examples/manager root@$line:/spark-examples/ && scp /spark-examples/workers root@$line:/spark-examples/
+        scp /spark-lab/hadoop-3.3.1.tar.gz root@$line:/spark-lab/ &&  scp /spark-lab/spark-3.2.1-bin-hadoop3.2.tgz root@$line:/spark-lab/ && scp /spark-lab/setup.py root@$line:/spark-lab/ && scp /spark-lab/manager root@$line:/spark-lab/ && scp /spark-lab/workers root@$line:/spark-lab/
         echo "Setup $line"
-        ssh root@$line -n "cd /spark-examples/ && python3 setup.py"
+        ssh root@$line -n "cd /spark-lab/ && python3 setup.py"
         echo "Finished config node $line"
     fi
 done
 
-manager=$(cat /spark-examples/manager)
+manager=$(cat /spark-lab/manager)
 echo "export SPARK_MASTER=$manager" > env.sh
